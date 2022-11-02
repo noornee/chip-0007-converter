ef10ca4b7b469bd9d997ca909097a2bf3dbadb49
noornee <noornee.011@gmail.com>
2022-11-02 18:02:01 +0100

    draftt

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
 func main() {
+	dataSchema := &CHIP{
+		Format:           "",
+		Name:             "",
+		Description:      "",
+		MintingTool:      "",
+		SensitiveContent: false,
+		SeriesNumber:     0,
+		SeriesTotal:      0,
+		Attributes:       []Attr1{},
+		Collection:       Collect{},
+		Data:             Dat{},
+	}
 }
