cb9c314411fbfa608d0daf4dca55040ba5133eac
noornee <noornee.011@gmail.com>
2022-11-02 21:29:36 +0100

    metadata schema

--- converter.go
+++ converter.go
@@ -1,36 +1,37 @@
 package main
 
-type CHIP struct {
-	Format           string     `csv:"format"`
-	Name             string     `csv:"name"`
-	Description      string     `csv:"description"`
-	MintingTool      string     `csv:"minting_tool"`
-	SensitiveContent bool       `csv:"sensitive_content"`
-	SeriesNumber     int        `csv:"series_number"`
-	SeriesTotal      int        `csv:"series_total"`
-	Attributes       []attr1    `csv:"attributes"`
-	Collection       collection `csv:"collection"`
-	Data             data       `csv:"data"`
+type MetaData struct {
+	Format           string     `json:"format"`
+	Name             string     `json:"name"`
+	Description      string     `json:"description"`
+	MintingTool      string     `json:"minting_tool"`
+	SensitiveContent bool       `json:"sensitive_content"`
+	SeriesNumber     int        `json:"series_number"`
+	SeriesTotal      int        `json:"series_total"`
+	Attributes       []attr1    `json:"attributes"`
+	Collection       collection `json:"collection"`
+	Data             data       `json:"data"`
+	Hash             string     `json:"hash"`
 }
 
 type attr1 struct {
-	TraitType string `csv:"trait_type"`
-	Value     string `csv:"value"`
-	MinValue  int    `csv:"min_value,omitempty"`
-	MaxValue  int    `csv:"max_value,omitempty"`
+	TraitType string `json:"trait_type"`
+	Value     string `json:"value"`
+	MinValue  int    `json:"min_value,omitempty"`
+	MaxValue  int    `json:"max_value,omitempty"`
 }
 
 type attr2 struct {
-	Type  string `csv:"type"`
-	Value string `csv:"value"`
+	Type  string `json:"type"`
+	Value string `json:"value"`
 }
 
 type collection struct {
-	Name       string  `csv:"name"`
-	ID         string  `csv:"id"`
-	Attributes []attr2 `csv:"attributes"`
+	Name       string  `json:"name"`
+	ID         string  `json:"id"`
+	Attributes []attr2 `json:"attributes"`
 }
 
 type data struct {
-	ExampleData string `csv:"example_data"`
+	ExampleData string `json:"example_data"`
 }
